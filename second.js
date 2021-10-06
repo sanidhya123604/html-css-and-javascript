426ae0cabcabc5a1c0893a3d2b83969cc093cfd4
sanidhya123604 <75679894+sanidhya123604@users.noreply.github.com>
2021-10-06 22:52:02 +0530

    Create second.js

---
+++ second.js
@@ -0,0 +1,21 @@
+let arr = [2,6,8,45,22]
+//console.log(arr)
+
+//arr.push(9);
+//console.log(arr)
+
+//arr.pop(9);
+//console.log(arr);
+
+//arr.reverse();
+//console.log(arr);
+
+//object
+let myname = {
+    a : "sanidhya",
+    b: 232,
+    c : "hello world",
+    d : [1,2,3,4,5]
+}
+console.log(myname)
+console.log(myname.d)
